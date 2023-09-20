8b8f6bfd96540572530d6c0d528ba5355d70daf7
MAHESH <122071980+maheXh@users.noreply.github.com>
2023-09-21 01:41:01 +0530

    univariate_reg.ipynb to univariate_reg.py

--- univariate_reg.py
+++ univariate_reg.py
@@ -80,3 +80,62 @@ def grad_Des(x, y, w, b, alpha=0.001, num_iter=10000):
 w, b = grad_Des(x_train, y_train, w, b, alpha=0.0000001, num_iter=10000000)
 print(w, b)
 print(f"the cost is {compute_cost(x_train,y_train,w,b)}")
+b = 1.1246608943305345
+# 11.953839202896042 1.1246608943305345
+f_wb = gen_f(x_train, w, b)
+plt.plot(x_train, f_wb)
+plt.scatter(x_train, y_train)
+
+
+def predict(x, w, b):
+    return x * w + b
+
+
+f_wb = gen_f(x_train, w, b)
+plt.plot(x_train, f_wb)
+plt.scatter(x_train, y_train)
+
+x_i = int(input("enter the house area"))
+y_hat = predict(x_i, w, b)
+
+plt.scatter(x_i, y_hat, c="r")
+plt.axvline(x=x_i, color="blue")
+plt.axhline(y=y_hat, color="blue")
+print(f"the predicted prce is {y_hat}")
+
+
+def compute_cost(x, y, w, b):
+    m = x.shape[0]
+    cost = 0
+    for i in range(m):
+        f_wb = predict(x[i], w, b)
+        cost += (f_wb - y[i]) ** 2
+    total_cost = cost * (1 / (2 * m))
+    return total_cost
+
+
+print(f"the cost is {compute_cost(x_train,y_train,w,b)}")
+
+
+def derivative(x, y, w, b):
+    m = x.shape[0]
+    sum_w, sum_b = 0, 0
+    for i in range(m):
+        sum_w += (predict(x[i], w, b) - y[i]) * x[i]
+        sum_b += predict(x[i], w, b) - y[i]
+    return sum_w * (1 / m), sum_b * (1 / m)
+
+
+def grad_Des(x, y, w, b, alpha=0.001, num_iter=10000):
+    for i in range(num_iter):
+        d_dw, d_db = derivative(x, y, w, b)
+        temp_w = w - alpha * (d_dw)
+        temp_b = b - alpha * (d_db)
+        w = temp_w
+        b = temp_b
+    return w, b
+
+
+w, b = grad_Des(x_train, y_train, w, b, alpha=0.0000001, num_iter=10000000)
+print(w, b)
+print(f"the cost is {compute_cost(x_train,y_train,w,b)}")
